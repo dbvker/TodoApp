b6e78e2f17b83cfbc60f3fd8ec92a912b68f146e
Dylan Baker <dylanmbaker@yahoo.com>
2023-07-06 18:16:03 -0500

    Completed the authentication controller

---
+++ TodoApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace TodoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+	private readonly IConfiguration _config;
+
+	public AuthenticationController(IConfiguration config)
+    {
+		_config = config;
+	}
+
+	public record AuthenticationData(string? Username, string Password);
+	public record UserData(int Id, string FirstName, string LastName, string Username);
+
+	[HttpPost("token")]
+	[AllowAnonymous]
+	public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
+	{
+		var user = ValidateCredentials(data);
+
+		if (user == null)
+		{
+			return Unauthorized();
+		}
+
+		string token = GenerateToken(user);
+
+		return Ok(token);
+	}
+
+	private string GenerateToken(UserData user)
+	{
+		var secretKey = new SymmetricSecurityKey(
+			Encoding.ASCII.GetBytes(
+				_config.GetValue<string>("Authentication:SecretKey")!));
+
+		var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+		List<Claim> claims = new();
+		claims.Add(new(JwtRegisteredClaimNames.Sub, user.Id.ToString()));
+		claims.Add(new(JwtRegisteredClaimNames.UniqueName, user.Username));
+		claims.Add(new(JwtRegisteredClaimNames.GivenName, user.FirstName));
+		claims.Add(new(JwtRegisteredClaimNames.FamilyName, user.LastName));
+
+		var token = new JwtSecurityToken(
+			_config.GetValue<string>("Authentication:Issuer"),
+			_config.GetValue<string>("Authentication:Audience"),
+			claims,
+			DateTime.UtcNow,
+			DateTime.UtcNow.AddMinutes(1),
+			signingCredentials);
+
+		return new JwtSecurityTokenHandler().WriteToken(token);
+	}
+
+	private UserData? ValidateCredentials(AuthenticationData data)
+	{
+		// This is not production code - Replace this with a call to your auth system
+		if (CompareValues(data.Username, "tcorey") &&
+			CompareValues(data.Password, "Test123"))
+		{
+			return new UserData(1, "Tim", "Corey", data.Username!);
+		}
+
+		if (CompareValues(data.Username, "sstorm") &&
+			CompareValues(data.Password, "Test123"))
+		{
+			return new UserData(2, "Sue", "Storm", data.Username!);
+		}
+
+		return null;
+	}
+
+	private bool CompareValues(string? actual, string expected)
+	{
+		if (actual == null)
+		{
+			if (actual!.Equals(expected))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
